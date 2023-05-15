dcd5821586d2959c7750aac5ce3800357ba830ed
Tejesh86 <121223428+Tejesh86@users.noreply.github.com>
2023-05-15 22:53:37 +0530

    Update streamlit_app.py
    inserting into snow

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,6 +53,9 @@ streamlit.dataframe(my_data_row)
 
 streamlit.header("Fruityvice Fruit Advice!");
 fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit');
+
 streamlit.write('Thanks for adding', fruit_choice);
 
+streamlit.write('Thanks for adding', add_my_fruit);
+my_cur.execute("insert into fruit_load_list values('from streamlit')");
 
