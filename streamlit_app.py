4301d1761ce8f0e1ceef1cba28c0d3a8ac1c1b23
Tejesh86 <121223428+Tejesh86@users.noreply.github.com>
2023-05-16 20:49:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,8 +43,8 @@ def get_fruitvice_data(this_fruit_choice)
  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
  return fruityvice_normalized
 
-back_from_function = get_fruitvice_data(this_fruit_choice) 
-streamlit.dataframeback_from_function 
+back_from_function = get_fruitvice_data(fruit_choice) 
+streamlit.dataframe(back_from_function)
 
 
 
