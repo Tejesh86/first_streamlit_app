1d458cedc92aae7fc05de703aa6b8a35c132c85b
Tejesh86 <121223428+Tejesh86@users.noreply.github.com>
2023-05-16 19:45:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import streamlit
 import pandas
 import requests
-import snowflake.connector
-from urllib.error import URLError
 
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
@@ -20,8 +19,9 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected];
 streamlit.dataframe(fruits_to_show);
 
-#streamlit.header("Build your own fruit Smoothie");
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon");
+ 
+streamlit.header("Fruityvice Fruit Advice!");
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon");
 # streamlit.text(fruityvice_response.json());
 
 # write your own comment -what does the next line do? 
@@ -29,27 +29,18 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi");
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi");
 
 # take the json version of the response and normalize it
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as a table
-#streamlit.dataframe(fruityvice_normalized);
+streamlit.dataframe(fruityvice_normalized);
 
 #New section to display fruitvice api response
-streamlit.header("Fruityvice Fruit Advice!")
-# try:
-#fruit_choice = streamlit.text_input('What fruit would you like information about?')
-# if not fruit_choice:
-   # streamlit.error("Please select a fruit to get information.")
-# else:
-  # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice);
-   # fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
- #   streamlit.dataframe(fruityvice_normalized);
- # except URLError as e:
- # streamlit.error()
-  
+streamlit.header("Fruityvice Fruit Advice!");
+fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi');
+streamlit.write('The user entered', fruit_choice);
 import requests
 
 # don't run anything past here while we troubleshoot
