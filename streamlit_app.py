0314ab3dd40894ca1737019c84e9d52314ee59ea
Tejesh86 <121223428+Tejesh86@users.noreply.github.com>
2023-05-16 19:46:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,12 +25,11 @@ streamlit.dataframe(fruits_to_show);
 # streamlit.text(fruityvice_response.json());
 
 # write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#---fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+#-----streamlit.dataframe(fruityvice_normalized)
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi");
+#---fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi");
 
 # take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
